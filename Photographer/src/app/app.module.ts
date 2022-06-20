14a1d5f156f76c25104fe0a68e3c908426eba216
DmnkHofmann <dmnkhofmann@gmail.com>
2022-06-20 11:52:06 +0200

    New Angular Project. Created components. First push.

---
+++ Photographer/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
+import { AwardsComponent } from './awards/awards.component';
+import { AboutComponent } from './about/about.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainComponent,
+    AwardsComponent,
+    AboutComponent,
+    ReviewsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
