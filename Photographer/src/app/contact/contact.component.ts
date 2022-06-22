7fe1786043aee10601e339dc75a2916dbb3f27da
IrisLinner <iris.linner@gmx.at>
2022-06-22 14:21:06 +0200

    Merge branch 'main' of https://github.com/DmnkHofmann/cfFE16-team4

--- Photographer/src/app/contact/contact.component.ts
+++ Photographer/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'contact',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  contact = new FormGroup({
+    firstName: new FormControl("",[Validators.required, Validators.minLength(3)]),
+    lastName: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    eMail: new FormControl("",Validators.required),
+    textArea: new FormControl("", [Validators.required, Validators.minLength(2)]),
+  })
+  
   constructor() { }
-
   ngOnInit(): void {
   }
 
+submitForm() {
+  if (this.contact.valid) {
+     var a = this.contact.value;
+  console.log(a);
+  }
+}
 }
